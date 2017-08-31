214b0a4b048dbe9f5addb4e3f799d479e41ac400
Nicolas Takashi <nicolas@setima.pt>
2017-08-31 19:09:10 -0300

    Added Logical Operators

---
+++ Flepper.Core/QueryBuilder/Operators/Logical/Interfaces/ILogicalOperators.cs
@@ -0,0 +1,8 @@
+﻿namespace Flepper.Core.QueryBuilder.Operators.Logical.Interfaces
+{
+    public interface ILogicalOperators
+    {
+        void And(string column);
+        void Or(string column);
+    }
+}
---
+++ Flepper.Tests.Unit/QueryBuilder/Commands/WhereFilterTests.cs
@@ -0,0 +1,78 @@
+﻿using Flepper.Core.QueryBuilder.Commands;
+using Flepper.Core.QueryBuilder.Commands.Extensions;
+using Flepper.Core.QueryBuilder.Filters.Extensions;
+using Flepper.Core.QueryBuilder.Operators.Comparison.Extensions;
+using Flepper.Core.QueryBuilder.Operators.Logical.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace Flepper.Tests.Unit.QueryBuilder.Commands
+{
+    [Collection("CommandTests")]
+    public class WhereFilterTests
+    {
+        [Fact]
+        public void ShouldContainsWhereInStatement()
+        {
+            var selectCommand = new SelectCommand();
+
+            selectCommand.Select().From("user").Where("name");
+
+            selectCommand.Query
+                .Trim()
+                .Should()
+                .Contain("WHERE");
+        }
+
+        [Fact]
+        public void ShouldContainsWhereWithComparisonOperatos()
+        {
+            var selectCommand = new SelectCommand();
+
+            selectCommand.Select()
+                .From("user")
+                .Where("name")
+                .Equal("gustavo");
+
+            selectCommand.Query
+                .Trim()
+                .Should()
+                .Contain("WHERE [name] = 'gustavo'");
+        }
+
+        [Fact]
+        public void ShouldContainsWhereWithLogicalOperator()
+        {
+            var selectCommand = new SelectCommand();
+
+            selectCommand.Select()
+                .From("user")
+                .Where("name")
+                .Equal("gustavo")
+                .And("age");
+
+            selectCommand.Query
+                .Trim()
+                .Should()
+                .Contain("WHERE [name] = 'gustavo' AND [age]");
+        }
+
+        [Fact]
+        public void ShouldContainsWhereWithLogicalOperatorWithComparisonOperators()
+        {
+            var selectCommand = new SelectCommand();
+
+            selectCommand.Select()
+                .From("user")
+                .Where("name")
+                .Equal("gustavo")
+                .And("age")
+                .Equal(26);
+
+            selectCommand.Query
+                .Trim()
+                .Should()
+                .Contain("WHERE [name] = 'gustavo' AND [age] = 26");
+        }
+    }
+}
