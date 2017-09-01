33c7d33c4807f8b3c5319200635a31720806442b
nicolastakashi <nicolas.tcs@hotmail.com>
2017-09-01 19:30:29 -0300

    Added FlepperQueryBuilder Wrapper

---
+++ Flepper.Core/QueryBuilder/FlepperQueryBuilder.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Flepper.Core.QueryBuilder
+{
+    public static class FlepperQueryBuilder
+    {
+        public static ISelectCommand Select()
+        {
+            return new SelectCommand().Select();
+        }
+
+        public static ISelectCommand Select(params string[] columns)
+        {
+            return new SelectCommand().Select(columns);
+        }
+
+        public static IDeleteCommand Delete()
+        {
+            return new DeleteCommand().Delete();
+        }
+    }
+}
