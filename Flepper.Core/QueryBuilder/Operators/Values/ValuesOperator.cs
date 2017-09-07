b5a36ccd26837323f9b61bd5996121ee67a9daee
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2017-09-07 00:57:46 -0300

    develop insert statement #4

---
+++ Flepper.Core/QueryBuilder/Operators/Values/ValuesOperator.cs
@@ -0,0 +1,20 @@
+﻿using Flepper.Core.QueryBuilder.Operators.Values.Interfaces;
+using Flepper.Core.Base;
+using System.Linq;
+
+namespace Flepper.Core.QueryBuilder.Operators.Values
+{
+    public class ValuesOperator : BaseFlepperQueryBuilder, IValuesOperator
+    {
+        public void Values(params string[] values)
+        {
+            var definedValues = values.Aggregate("", (current, value) => current + $"{value},");
+            definedValues = definedValues.Remove(definedValues.Length - 1, 1) + "";
+
+            Command.AppendFormat("VALUES ({0})", definedValues);
+
+        }
+
+        
+    }
+}
---
+++ Flepper.Tests.Unit/QueryBuilder/Commands/InsertCommandTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Flepper.Core.QueryBuilder.Commands.Extensions;
+using Xunit;
+using Flepper.Core.QueryBuilder;
+
+namespace Flepper.Tests.Unit.QueryBuilder.Commands
+{
+    [Collection("CommandTests")]
+    public class InsertCommandTests
+    {
+
+        [Fact]
+        public void ShouldCreateInsertStatementWithTable()
+        {
+            FlepperQueryBuilder.InsertInto("Test");
+
+            FlepperQueryBuilder
+                .Query
+                .Trim()
+                .Should()
+                .Be("INSERT INTO [Test]");
+        }
+
+        [Fact]
+        public void ShouldCreateInsertStatementWithColumns()
+        {
+            FlepperQueryBuilder
+                .InsertInto("Test", "column1", "column2");
+
+            FlepperQueryBuilder.Query
+                .Trim()
+                .Should()
+                .Be("INSERT INTO [Test] (column1,column2)");
+        }
+
+        [Fact]
+        public void ShouldCreateInsertStatementWithValuesToColumns()
+        {
+            FlepperQueryBuilder
+            .InsertInto("Test", "column1", "column2")
+            .Values("'value1'","2");
+
+            FlepperQueryBuilder.Query
+                .Trim()
+                .Should()
+                .Be("INSERT INTO [Test] (column1,column2) VALUES ('value1',2)");
+
+        }
+
+        [Fact]
+        public void ShouldCreateInsertStatementWithValues()
+        {
+            FlepperQueryBuilder
+            .InsertInto("Test")
+            .Values("'value1'", "2");
+
+            FlepperQueryBuilder.Query
+                .Trim()
+                .Should()
+                .Be("INSERT INTO [Test] VALUES ('value1',2)");
+
+        }
+    }
+}
