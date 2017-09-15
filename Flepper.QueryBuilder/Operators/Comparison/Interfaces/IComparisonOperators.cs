5d0ae85a61dc5d20117e77f24950ccc8ac079c0e
Nicolas Takashi <nicolas.tcs@hotmail.com>
2017-09-15 07:44:27 -0300

    Update IComparisonOperators.cs

--- Flepper.QueryBuilder/Operators/Comparison/Interfaces/IComparisonOperators.cs
+++ Flepper.QueryBuilder/Operators/Comparison/Interfaces/IComparisonOperators.cs
@@ -8,42 +8,42 @@
         /// <summary>
         /// Equal Comparison Operator Contract
         /// </summary>
-        /// <param name="value">Value</param>
+        /// <param name="value">Value used to compare</param>
         /// <returns></returns>
         IComparisonOperators EqualTo(object value);
 
         /// <summary>
         /// Greater Than Comparison Operator Contract
         /// </summary>
-        /// <param name="value">Value</param>
+        /// <param name="value">Value used to compare</param>
         /// <returns></returns>
         IComparisonOperators GreaterThan(int value);
 
         /// <summary>
         /// Less Than Comparison Operator Contract
         /// </summary>
-        /// <param name="value">Value</param>
+        /// <param name="value">Value used to compare</param>
         /// <returns></returns>
         IComparisonOperators LessThan(int value);
 
         /// <summary>
         /// Greater Than Or Equal Comparison Operator Contract
         /// </summary>
-        /// <param name="value">Value</param>
+        /// <param name="value">Value used to compare</param>
         /// <returns></returns>
         IComparisonOperators GreaterThanOrEqualTo(int value);
 
         /// <summary>
         /// Less Than Or Equal Comparison Operator Contract
         /// </summary>
-        /// <param name="value">Value</param>
+        /// <param name="value">Value used to compare</param>
         /// <returns></returns>
         IComparisonOperators LessThanOrEqualTo(int value);
 
         /// <summary>
         /// Not equal Comparison Operator Contract
         /// </summary>
-        /// <param name="value">Value</param>
+        /// <param name="value">Value used to compare</param>
         /// <returns></returns>
         IComparisonOperators NotEqualTo(object value);
     }
