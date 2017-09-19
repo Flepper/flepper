3d01f144d3200c53a90bd0c8010270457da8c2ce
nicolastakashi <nicolas.tcs@hotmail.com>
2017-09-19 20:14:47 -0300

    Added Dapper

---
+++ Flepper.QueryBuilder.DapperExtensions/Extensions/DbConnectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Flepper.QueryBuilder.DapperExtensions
+{
+    public static class DbConnectionExtensions
+    {
+        public static IQueryCommand FlepperQuery(this IDbConnection dbConnection) => new FlepperDapperQuery(dbConnection);
+    }
+}
