f51c798767dc942a1d4a0fe0486634e33ce6ea41
nicolastakashi <nicolas.tcs@hotmail.com>
2017-09-19 20:14:47 -0300

    Added Dapper

---
+++ Flepper.QueryBuilder.DapperExtensions/Extensions/QueryBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+
+namespace Flepper.QueryBuilder.DapperExtensions
+{
+    public static class QueryBuilderExtensions
+    {
+        public static int Execute(this IQueryCommand queryCommand, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+            => queryCommand.Execute(transaction, commandTimeout, commandType);
+
+        public static Task<int> ExecuteAsync(this IQueryCommand queryCommand, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+            => queryCommand.ExecuteAsync(transaction, commandTimeout, commandType);
+    }
+}
---
+++ Flepper.QueryBuilder.DapperExtensions/FlepperDapperQuery.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Flepper.QueryBuilder.DapperExtensions
+{
+    internal class FlepperDapperQuery : QueryBuilder
+    {
+        private readonly IDbConnection dbConnection;
+
+        public FlepperDapperQuery(IDbConnection dbConnection)
+        {
+            this.dbConnection = dbConnection;
+        }
+
+        public int Execute(IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            var queryResult = BuildWithParameters();
+            return dbConnection.Execute(queryResult.Query, queryResult.Parameters, transaction, commandTimeout, commandType);
+        }
+
+        public Task<int> ExecuteAsync(IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            var queryResult = BuildWithParameters();
+            return dbConnection.ExecuteAsync(queryResult.Query, queryResult.Parameters, transaction, commandTimeout, commandType);
+        }
+
+        public IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn> map,
+            object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+            => dbConnection.Query(Build(), map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+
+        public IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TReturn> map,
+            object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+            => dbConnection.Query(Build(), map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+
+        public IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TReturn> map,
+            object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+            => dbConnection.Query(Build(), map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+    }
+}
