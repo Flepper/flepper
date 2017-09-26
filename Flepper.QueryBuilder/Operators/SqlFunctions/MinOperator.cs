b1313c9fe9c611a9387423171c0edae1b21f6333
Gustavo Ferraz Fontes <gustavo.fontes@lambda3.com.br>
2017-09-26 15:29:11 -0300

    created min operator class

---
+++ Flepper.QueryBuilder/Operators/SqlFunctions/MinOperator.cs
@@ -0,0 +1,42 @@
+using System;
+using Flepper.QueryBuilder.Base;
+using static System.String;
+
+
+/// <summary>
+    /// MIN Operator class
+    /// </summary>
+public sealed class MinOperator : SqlColumn
+{
+    
+     /// <summary>
+        /// constructor to Count class
+        /// </summary>
+        /// <param name="column">column name</param>
+        /// <param name="alias">alias to column. All alias start with func_</param>
+        public MinOperator(string column, string alias) : base(column)
+        {
+            if (IsNullOrWhiteSpace(column) || IsNullOrWhiteSpace(alias)) throw new ArgumentNullException($"{nameof(column)} and {nameof(alias)} cannot be null or empty");
+
+            Column = $"MIN([{column}]) AS {alias}";
+        }
+
+    private MinOperator(string column) : base(column)
+    {
+        
+    }
+
+    /// <summary>
+        /// implicit operator to MinOperator
+        /// </summary>
+        /// <param name="column">column name</param>
+        public static implicit operator MinOperator(string column)
+            => new MinOperator(column);
+
+        /// <summary>
+        /// Overwrite ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+            => Column;
+}
