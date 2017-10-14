ac02ab0150e1c367ab49140fa097c3cb742159c1
nicolastakashi <nicolas.tcs@hotmail.com>
2017-10-13 22:00:06 -0300

    Added Paginations methods #67

---
+++ Flepper.QueryBuilder/Operators/Paginate/Interfaces/IFetchOperator.cs
@@ -0,0 +1,15 @@
+﻿namespace Flepper.QueryBuilder
+{
+    /// <summary>
+    /// Fetch Operator Interface
+    /// </summary>
+    public interface IFetchOperator : IQueryCommand
+    {
+        /// <summary>
+        /// Fetch Operator Contract
+        /// </summary>
+        /// <param name="numberOfRows">Number of rows returned</param>
+        /// <returns></returns>
+        IFetchOperator Fetch(int numberOfRows);
+    }
+}
